766ae0ac1bc4fe0ab34ab7c4219ba29be9a6c244
Atik2788 <ashanto653.2@gmail.com>
2022-12-08 23:41:08 +0600

    after doing floting and intro part

--- src/Component/Intro/Intro.js
+++ src/Component/Intro/Intro.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './Intro.css'
+import github from '../../img/github.png';
+import linkedin from '../../img/linkedin.png'
+import facebook from '../../img/Facebook.png'
+import vector1 from '../../img/Vector1.png'
+import vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import thumb from '../../img/thumbup.png'
+import crown from '../../img/crown.png'
+import glassimogi from '../../img/glassesimoji.png'
+import FlotingDiv from '../FlotingDiv/FlotingDiv';
 
 const Intro = () => {
     return (
@@ -7,14 +17,33 @@ const Intro = () => {
             <div className='i-left'>
                 <div className="i-name">
                     <span>Hello! I Am</span>
-                    <span>Atik</span>
+                    <span>Atikur Rahman</span>
                     <span>Web Developer with skills in React JS, JavaScript, MongoDB, Node JS, HTML, CSS, Bootstrap, and Tailwind.</span>
                 </div>
+                <button className='button i-button'>Hire Me</button>
+
+                <div className="i-icons">
+                    <img src={github} alt="" />
+                    <img src={linkedin} alt="" />
+                    <img src={facebook} alt="" />
+                </div>
+
             </div>
-            <button className='button i-button'>Hire Me</button>
 
             <div className='i-right'>
-                I am the right side
+                <img src={vector1} alt="" />
+                <img src={vector2} alt="" />
+                <img src={boy} alt="" />
+                <img src={glassimogi} alt="" />
+
+                <div style={{top: '-4%', left: '68%'}}>
+                    <FlotingDiv image={crown} text1={'Web'} text2={'Developer'}></FlotingDiv>
+                </div>
+
+                <div style={{top: '64%', left: '0rem'}}>
+                    <FlotingDiv image={thumb} text1={'Best Design'} text2={"Award"}></FlotingDiv>
+                </div>
+                
             </div>
         </div>
     );
